99bcc527d6993e87affd6b04a2252f4985e107a0
George Patchett <george.patchett@gmail.com>
2025-02-15 19:14:48 +0100

    WIP v2 companion integration

--- panel.js
+++ panel.js
@@ -7,17 +7,40 @@ const UpdateFeedbacks = require('./feedbacks')
 const UpdateVariableDefinitions = require('./variables')
 
 /**
- * @typedef {Object} companionEventPayload
- * @property {String} channelName
- * @property {Number} channelID
- * @property {"talkStatusChange" | "listenStatusChange" | "volumeChange"} event
- * @property {Boolean} [talking]
- * @property {Boolean} [listening]
- * @property {Number} [volume]
- *
- * @typedef {Object} companionEvent
- * @property {String} event
- * @property {companionEventPayload} payload
+ * @typedef CompanionEventPayload
+ * @param {string} event
+ * @param {Record<string, Channel>} state
+ * @param {string} [roleId]
+ */
+
+/**
+ * @typedef Channel
+ * @param {TemplateChannel} templateChannel
+ * @param {'disabled' | 'listenOnly' | 'talkOnly' | 'duplex'} localPermissions
+ * @param {boolean} talkActivity
+ * @param {number} volume
+ * @param {boolean} listenActive
+ * @param {boolean} talkActive
+ */
+
+/**
+ * @typedef TemplateChannel
+ * @param {string} channelId
+ * @param {string} channelName
+ * @param {number} orderIndex
+ * @param {string} [sendTrackId]
+ * @param {Record<string, RemoteIntercomParticipant>} remoteIntercomParticipants
+ */
+
+/**
+ * @typedef RemoteIntercomParticipant
+ * @param {string} userId
+ * @param {string} userName
+ * @param {boolean} talking
+ * @param {boolean} listening
+ * @param {number} volume
+ * @param {string} trackId
+ * @param {'disabled' | 'listenOnly' | 'talkOnly' | 'duplex'} remotePermissions
  */
 
 class PanelInstance extends InstanceBase {
@@ -33,16 +56,16 @@ class PanelInstance extends InstanceBase {
 		this.roleChoices = []
 		this.state = {}
 		this.updateStatus('connecting')
-		
+
 		// Get env variables from server
 		const { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY } = await fetch('https://www.webcomms.net/supabaseEnv').then((res) => res.json())
 		this.log('debug', 'Received env variables from server')
 		this.log('debug', PUBLIC_SUPABASE_URL)
 		this.log('debug', PUBLIC_SUPABASE_KEY)
-		
+
 		this.supabase = supabase.createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY)
 		this.log('debug', 'Created supabase client')
-		
+
 		// Check if user ID is set
 		if (this.config.companionIdentity !== '') {
 			const userExists = await this.supabase
@@ -68,45 +91,9 @@ class PanelInstance extends InstanceBase {
 			this.channel = this.supabase.channel(this.config.intercomName)
 
 			this.log('info', 'Configuring functions')
-			this.channel.on('broadcast', { event: this.config.companionIdentity }, (msg) => {
-				switch (msg.payload.event) {
-					case 'talkStatusChange':
-						this.log('info', 'Talk status change event received')
-						this.state[msg.payload.channelID].talking = msg.payload.talking
-						this.checkFeedbacks('talkActive')
-						break
 
-					case 'listenStatusChange':
-						this.log('info', 'Listen status change event received')
-						this.state[msg.payload.channelID].listening = msg.payload.listening
-						this.checkFeedbacks('listenActive')
-						break
 
-					case 'volumeChange':
-						this.log('info', 'Volume change event received')
-						this.state[msg.payload.channelID].volume = msg.payload.volume
-						this.setVariableValues({ ['volume' + msg.payload.channelID]: msg.payload.volume })
-						break
-
-					case 'companionSyncResponse':
-						this.log('info', 'Companion sync response received')
-						this.state = msg.payload.state
-						this.log('debug', JSON.stringify(msg.payload.state))
-						this.updateActions()
-						this.updateFeedbacks()
-						this.checkFeedbacks('talkActive', 'listenActive')
-						this.updateVariableDefinitions()
-						this.updateStatus('ok')
-						break
-
-					case 'companionSyncRequest':
-						break
-
-					default:
-						this.log('info', 'Unknown event received: ' + msg.payload.event)
-						break
-				}
-			})
+			this.channel.on('broadcast', { event: this.config.companionIdentity }, this.handleSupabaseBroadcast)
 
 			this.log('info', 'Listening to intercom')
 			this.channel.subscribe()
@@ -120,7 +107,7 @@ class PanelInstance extends InstanceBase {
 			// Get intercom config from supabase
 			this.log('info', 'Attempting to fetch intercom configuration')
 			this.supabaseIntercomConfig = await this.supabase
-				.from('intercoms')
+				.from('intercomsv2')
 				.select('*')
 				.eq('name', this.config.intercomName)
 				.single()
@@ -131,18 +118,31 @@ class PanelInstance extends InstanceBase {
 				return
 			}
 
-			this.intercomConfig = this.supabaseIntercomConfig.data.config.config
+			this.intercomConfig = this.supabaseIntercomConfig.data
+
 			this.log('info', 'Intercom configuration fetched and loaded')
 
 			// Set up channel choices
-			this.intercomConfig.channels.map((ch, index) => {
-				this.channelChoices.push({ id: index, label: ch })
+			Object.values(this.intercomConfig.templateChannels).map((ch, index) => {
+				this.channelChoices.push({ id: ch.channelId, label: ch.channelName })
+			})
+
+			this.channelChoices.sort((a, b) => {
+				if (a.label < b.label) return -1
+				if (a.label > b.label) return 1
+				return 0
 			})
 			this.log('info', 'Channel choices set')
 
 			// Set up role choices
-			this.intercomConfig.roles.map((role, index) => {
-				this.roleChoices.push({ id: index, label: role })
+			Object.values(this.intercomConfig.matrix).map((role, index) => {
+				this.roleChoices.push({ id: role.roleId, label: role.roleName })
+			})
+
+			this.roleChoices.sort((a, b) => {
+				if (a.label < b.label) return -1
+				if (a.label > b.label) return 1
+				return 0
 			})
 			this.log('info', 'Role choices set')
 		} else {
@@ -213,6 +213,51 @@ class PanelInstance extends InstanceBase {
 		]
 	}
 
+	/**
+	 * 
+	 * @param {CompanionEventPayload} broadcastEvent 
+	 */
+	handleSupabaseBroadcast(broadcastEvent) {
+		this.log('warn', broadcastEvent)
+		switch (broadcastEvent.payload.event) {
+			case 'talkStatusChange':
+				this.log('info', 'Talk status change event received')
+				this.state[broadcastEvent.payload.channelID].talking = broadcastEvent.payload.talking
+				this.checkFeedbacks('talkActive')
+				break
+
+			case 'listenStatusChange':
+				this.log('info', 'Listen status change event received')
+				this.state[broadcastEvent.payload.channelID].listening = broadcastEvent.payload.listening
+				this.checkFeedbacks('listenActive')
+				break
+
+			case 'volumeChange':
+				this.log('info', 'Volume change event received')
+				this.state[broadcastEvent.payload.channelID].volume = broadcastEvent.payload.volume
+				this.setVariableValues({ ['volume' + broadcastEvent.payload.channelID]: broadcastEvent.payload.volume })
+				break
+
+			case 'companionSyncResponse':
+				this.log('info', 'Companion sync response received')
+				this.state = broadcastEvent.payload.state
+				this.log('debug', JSON.stringify(broadcastEvent.payload.state))
+				this.updateActions()
+				this.updateFeedbacks()
+				this.checkFeedbacks('talkActive', 'listenActive')
+				this.updateVariableDefinitions()
+				this.updateStatus('ok')
+				break
+
+			case 'companionSyncRequest':
+				break
+
+			default:
+				this.log('info', 'Unknown event received: ' + broadcastEvent.payload.event)
+				break
+		}
+	}
+
 	updateActions() {
 		UpdateActions(this)
 	}
@@ -224,6 +269,7 @@ class PanelInstance extends InstanceBase {
 	updateVariableDefinitions() {
 		UpdateVariableDefinitions(this)
 	}
+
 }
 
 runEntrypoint(PanelInstance, UpgradeScripts)
