ecdd2f945b9f0216e2ffceff1fd1b75a73f7fb0d
George TGI WSL <george.patchett@gmail.com>
2026-08-04 15:18:07 +0200

    fix: C5 UpgradeScripts is empty

--- src/upgrades.ts
+++ src/upgrades.ts
@@ -1,4 +1,9 @@
-import type { CompanionStaticUpgradeScript } from '@companion-module/base'
+import type {
+	CompanionStaticUpgradeProps,
+	CompanionStaticUpgradeResult,
+	CompanionStaticUpgradeScript,
+	CompanionUpgradeContext,
+} from '@companion-module/base'
 import type { ModuleConfig } from './config.js'
 
 export const UpgradeScripts: CompanionStaticUpgradeScript<ModuleConfig>[] = [
@@ -13,4 +18,27 @@ export const UpgradeScripts: CompanionStaticUpgradeScript<ModuleConfig>[] = [
 	// 		updatedFeedbacks: [],
 	// 	}
 	// },
+
+	function upgradeToV2(
+		_: CompanionUpgradeContext<ModuleConfig>,
+		props: CompanionStaticUpgradeProps<ModuleConfig, undefined>,
+	): CompanionStaticUpgradeResult<ModuleConfig> {
+		const updatedActions = props.actions.map((action) => {
+			if (action.actionId === 'toggleTalk') {
+				return { ...action, actionId: 'toggleChannelInput' }
+			} else if (action.actionId === 'toggleListen') {
+				return { ...action, actionId: 'toggleChannelOutput' }
+			} else if (action.actionId === 'setVolume') {
+				return { ...action, actionId: 'setChannelVolume' }
+			} else return action
+		})
+
+		return {
+			updatedConfig: {
+				port: 7171,
+			},
+			updatedActions,
+			updatedFeedbacks: [],
+		}
+	},
 ]
