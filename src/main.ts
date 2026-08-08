623594a8081370fc40f1b49e29b0d33db951ee23
George TGI WSL <george.patchett@gmail.com>
2026-08-08 16:43:38 +0200

    fix: port clashes on module restart due to old listeners not being removed upon destroy

--- src/main.ts
+++ src/main.ts
@@ -92,6 +92,7 @@ export default class ModuleInstance extends InstanceBase<ModuleConfig> {
 	// When module gets deleted
 	async destroy(): Promise<void> {
 		this.server.closeAllConnections()
+		this.server.removeAllListeners()
 		clearInterval(this.companionSyncTimeout)
 		this.log('debug', 'destroy')
 	}
